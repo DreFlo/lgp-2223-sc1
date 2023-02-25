ae20f4bb3fd095d5f133eaa607aed4c46872b9e1
melisilva <melissa.silva2001.ms@gmail.com>
2023-02-25 11:05:41 +0000

    Add experimental flutter app with animations

---
+++ src/lib/animation_test/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:src/animation_test/screens/onboding/onboding_screen.dart';
+
+
+
+class MainScreenAni extends StatelessWidget {
+  const MainScreenAni({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'The Flutter Way',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Color.fromARGB(255, 22, 17, 39),
+        primarySwatch: Colors.blue,
+        fontFamily: "Intel",
+        inputDecorationTheme: InputDecorationTheme(
+          filled: true,
+          fillColor: Colors.white,
+          errorStyle: TextStyle(height: 0),
+          border: defaultInputBorder,
+          enabledBorder: defaultInputBorder,
+          focusedBorder: defaultInputBorder,
+          errorBorder: defaultInputBorder,
+        ),
+      ),
+      home: const OnbodingScreen(),
+    );
+  }
+}
+
+const defaultInputBorder = OutlineInputBorder(
+  borderRadius: BorderRadius.all(Radius.circular(16)),
+  borderSide: BorderSide(
+    color: Color(0xFFDEE3F2),
+    width: 1,
+  ),
+);
---
+++ src/lib/animation_test/screens/home/components/course_card.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class CourseCard extends StatelessWidget {
+  const CourseCard({
+    Key? key,
+    required this.title,
+    required this.description,
+    this.color = const Color(0xFF7553F6),
+  }) : super(key: key);
+
+  final String title, description;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 24),
+      height: 280,
+      width: 260,
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.all(Radius.circular(30)),
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 6, right: 8),
+              child: Column(
+                children: [
+                  Text(
+                    title,
+                    style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                        color: Colors.white, fontWeight: FontWeight.w600),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 12, bottom: 8),
+                    child: Text(
+                      description,
+                      style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                        color: Colors.white, fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
