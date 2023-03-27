7c73ae2a19d72dccbc5a34564b5fcf3c8b032343
André Flores <andrejesusferflores@gmail.com>
2023-03-27 22:26:17 +0100

    Add DAOs to unit test service locator

--- src/test/utils/service_locator_test_util.dart
+++ src/test/utils/service_locator_test_util.dart
@@ -106,4 +106,62 @@ void setupServiceLocatorUnitTests() {
           .inMemoryDatabaseBuilder()
           .addCallback(callback)
           .build());
+
+  serviceLocator.registerSingletonAsync<PersonDao>(
+      () async => serviceLocator.get<AppDatabase>().personDao,
+      dependsOn: [AppDatabase]);
+  serviceLocator.registerSingletonAsync<InstitutionDao>(
+      () async => serviceLocator.get<AppDatabase>().institutionDao,
+      dependsOn: [AppDatabase]);
+  serviceLocator.registerSingletonAsync<SubjectDao>(
+      () async => serviceLocator.get<AppDatabase>().subjectDao,
+      dependsOn: [AppDatabase]);
+  serviceLocator.registerSingletonAsync<TaskGroupDao>(
+      () async => serviceLocator.get<AppDatabase>().taskGroupDao,
+      dependsOn: [AppDatabase]);
+  serviceLocator.registerSingletonAsync<TaskDao>(
+      () async => serviceLocator.get<AppDatabase>().taskDao,
+      dependsOn: [AppDatabase]);
+  serviceLocator.registerSingletonAsync<NoteDao>(
+      () async => serviceLocator.get<AppDatabase>().noteDao,
+      dependsOn: [AppDatabase]);
+  serviceLocator.registerSingletonAsync<EvaluationDao>(
+      () async => serviceLocator.get<AppDatabase>().evaluationDao,
+      dependsOn: [AppDatabase]);
+  serviceLocator.registerSingletonAsync<MediaDao>(
+      () async => serviceLocator.get<AppDatabase>().mediaDao,
+      dependsOn: [AppDatabase]);
+  serviceLocator.registerSingletonAsync<VideoDao>(
+      () async => serviceLocator.get<AppDatabase>().videoDao,
+      dependsOn: [AppDatabase]);
+  serviceLocator.registerSingletonAsync<SeriesDao>(
+      () async => serviceLocator.get<AppDatabase>().seriesDao,
+      dependsOn: [AppDatabase]);
+  serviceLocator.registerSingletonAsync<BookDao>(
+      () async => serviceLocator.get<AppDatabase>().bookDao,
+      dependsOn: [AppDatabase]);
+  serviceLocator.registerSingletonAsync<SeasonDao>(
+      () async => serviceLocator.get<AppDatabase>().seasonDao,
+      dependsOn: [AppDatabase]);
+  serviceLocator.registerSingletonAsync<ReviewDao>(
+      () async => serviceLocator.get<AppDatabase>().reviewDao,
+      dependsOn: [AppDatabase]);
+  serviceLocator.registerSingletonAsync<EpisodeDao>(
+      () async => serviceLocator.get<AppDatabase>().episodeDao,
+      dependsOn: [AppDatabase]);
+  serviceLocator.registerSingletonAsync<MovieDao>(
+      () async => serviceLocator.get<AppDatabase>().movieDao,
+      dependsOn: [AppDatabase]);
+  serviceLocator.registerSingletonAsync<BookNoteDao>(
+      () async => serviceLocator.get<AppDatabase>().bookNoteDao,
+      dependsOn: [AppDatabase]);
+  serviceLocator.registerSingletonAsync<EpisodeNoteDao>(
+      () async => serviceLocator.get<AppDatabase>().episodeNoteDao,
+      dependsOn: [AppDatabase]);
+  serviceLocator.registerSingletonAsync<TaskNoteDao>(
+      () async => serviceLocator.get<AppDatabase>().taskNoteDao,
+      dependsOn: [AppDatabase]);
+  serviceLocator.registerSingletonAsync<SubjectNoteDao>(
+      () async => serviceLocator.get<AppDatabase>().subjectNoteDao,
+      dependsOn: [AppDatabase]);
 }
