ba0d1faea4eacce507d176462908be010b361d52
lessthelonely <42583814+lessthelonely@users.noreply.github.com>
2023-03-28 19:06:52 +0100

    Adding color theme file.

--- src/lib/pages/leisure/media_page.dart
+++ src/lib/pages/leisure/media_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:src/animation_test/main.dart';
 import 'package:src/daos/person_dao.dart';
 import 'package:src/models/person.dart';
+import 'package:src/themes/colors.dart';
 import 'package:src/utils/service_locator.dart';
 
 class MyMediaPage extends StatelessWidget {
@@ -34,70 +35,86 @@ class MyMediaPage extends StatelessWidget {
       height: 1500,
       child: Column(children: [
         Row(children: [
-            Stack(children: [
-              Stack(
-                  clipBehavior: Clip.antiAlias,
-                  alignment: AlignmentDirectional.topCenter,
-                  children: [
-                    Positioned(
-                        top: 20,
-                        child: Container(
-                          width: 115,
-                          height: 16,
-                          alignment: Alignment.center,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            color: Color(0xFF414554),
-                          ),
-                          child: Text(
-                            this.type.toUpperCase(),
-                            style: Theme.of(context).textTheme.headlineMedium,
-                            textAlign: TextAlign.center,
-                          ),
-                        )),
-                    SizedBox(
-                      height: MediaQuery.of(context).size.height * 0.4,
-                      width: MediaQuery.of(context).size.width,
-                      child: ShaderMask(
-                        shaderCallback: (rect) {
-                          return LinearGradient(
-                            begin: Alignment.topCenter,
-                            end: Alignment.bottomCenter,
-                            stops: [0.01, 0.5, 0.9],
-                            colors: [
-                              Color(0xFF22252D),
-                              Colors.transparent,
-                              Color(0xFF22252D)
-                            ],
-                          ).createShader(
-                              Rect.fromLTRB(0, 0, rect.width, rect.height));
-                        },
-                        blendMode: BlendMode.dstOut,
-                        child: Image.asset(
-                          'assets/images/poster.jpg',
-                          fit: BoxFit.fitWidth,
+          Stack(children: [
+            Stack(
+                clipBehavior: Clip.antiAlias,
+                alignment: AlignmentDirectional.topCenter,
+                children: [
+                  Positioned(
+                      top: 20,
+                      child: Container(
+                        width: 115,
+                        height: 16,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Color(0xFF414554),
                         ),
+                        child: Text(
+                          this.type.toUpperCase(),
+                          style: Theme.of(context).textTheme.headlineMedium,
+                          textAlign: TextAlign.center,
+                        ),
+                      )),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.4,
+                    width: MediaQuery.of(context).size.width,
+                    child: ShaderMask(
+                      shaderCallback: (rect) {
+                        return LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          stops: [0.01, 0.5, 0.9],
+                          colors: [
+                            Color(0xFF22252D),
+                            Colors.transparent,
+                            Color(0xFF22252D)
+                          ],
+                        ).createShader(
+                            Rect.fromLTRB(0, 0, rect.width, rect.height));
+                      },
+                      blendMode: BlendMode.dstOut,
+                      child: Image.asset(
+                        'assets/images/poster.jpg',
+                        fit: BoxFit.fitWidth,
                       ),
                     ),
-                  ]),
-              Positioned(
-                top: 250,
-                child: SizedBox(
-                    height: 500,
-                    width: MediaQuery.of(context).size.width * 0.85,
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 16.0, right: 16.0),
-                      child: Text(
-                        this.title.toUpperCase(),
-                        softWrap: true,
-                        textWidthBasis: TextWidthBasis.longestLine,
-                        style: Theme.of(context).textTheme.titleLarge,
-                        textAlign: TextAlign.left,
-                        maxLines: 5,
-                      ),
-                    )),
-              )
-            ])
+                  ),
+                ]),
+            Positioned(
+                top: 225,
+                child: Row(
+                  children: [
+                    Column(children: [
+                      SizedBox(
+                          height: 500,
+                          width: MediaQuery.of(context).size.width * 0.85,
+                          child: Padding(
+                            padding:
+                                const EdgeInsets.only(left: 16.0, right: 16.0),
+                            child: Text(
+                              this.title.toUpperCase(),
+                              softWrap: true,
+                              textWidthBasis: TextWidthBasis.longestLine,
+                              style: Theme.of(context).textTheme.titleLarge,
+                              textAlign: TextAlign.left,
+                              maxLines: 5,
+                            ),
+                          )),
+                    ]),
+                    Column(
+                      children: [
+                        IconButton(
+                            color: leisureColor,
+                            onPressed: () {
+                              //TODO: add to favorites
+                            },
+                            icon: Icon(Icons.favorite_border))
+                      ],
+                    )
+                  ],
+                ))
+          ])
         ]),
         Row(children: [Text('HELLO')]),
       ]),
---
+++ src/lib/themes/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color(0xFF6C5DD3);
+
+
+const Color leisureColor = Color(0xFFF52349);
