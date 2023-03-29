b6bda08c24ab0fdd00b5e26a25cc31150a737a24
lessthelonely <42583814+lessthelonely@users.noreply.github.com>
2023-03-29 15:45:45 +0100

    Finished Media page.

--- src/lib/pages/leisure/media_page.dart
+++ src/lib/pages/leisure/media_page.dart
@@ -53,191 +53,188 @@ class MyMediaPage extends StatelessWidget {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return Container(
-        constraints: BoxConstraints.expand(height: 1000),
-        child: Column(children: [
-          Row(children: [
+    return Wrap(spacing: 10, children: [
+      Row(children: [
+        Stack(
+          alignment: AlignmentDirectional.bottomStart,
+          children: [
             Stack(
-              alignment: AlignmentDirectional.bottomStart,
-              children: [
-                Stack(
-                    clipBehavior: Clip.antiAlias,
-                    alignment: AlignmentDirectional.topCenter,
-                    children: [
-                      Positioned(
-                          top: 20,
-                          child: Container(
-                            width: 115,
-                            height: 18,
-                            alignment: Alignment.center,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              color: Color(0xFF414554),
-                            ),
-                            child: Text(
-                              this.type.toUpperCase(),
-                              style: Theme.of(context).textTheme.headlineMedium,
-                              textAlign: TextAlign.center,
-                            ),
-                          )),
-                      SizedBox(
-                        height: MediaQuery.of(context).size.height * 0.4,
-                        width: MediaQuery.of(context).size.width,
-                        child: ShaderMask(
-                          shaderCallback: (rect) {
-                            return LinearGradient(
-                              begin: Alignment.topCenter,
-                              end: Alignment.bottomCenter,
-                              stops: [0.01, 0.5, 0.9],
-                              colors: [
-                                Color(0xFF22252D),
-                                Colors.transparent,
-                                Color(0xFF22252D)
-                              ],
-                            ).createShader(
-                                Rect.fromLTRB(0, 0, rect.width, rect.height));
-                          },
-                          blendMode: BlendMode.dstOut,
-                          child: Image.asset(
-                            'assets/images/poster.jpg',
-                            fit: BoxFit.fitWidth,
-                          ),
+                clipBehavior: Clip.antiAlias,
+                alignment: AlignmentDirectional.topCenter,
+                children: [
+                  Positioned(
+                      top: 20,
+                      child: Container(
+                        width: 115,
+                        height: 18,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Color(0xFF414554),
                         ),
+                        child: Text(
+                          this.type.toUpperCase(),
+                          style: Theme.of(context).textTheme.headlineMedium,
+                          textAlign: TextAlign.center,
+                        ),
+                      )),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.4,
+                    width: MediaQuery.of(context).size.width,
+                    child: ShaderMask(
+                      shaderCallback: (rect) {
+                        return LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          stops: [0.01, 0.5, 0.9],
+                          colors: [
+                            Color(0xFF22252D),
+                            Colors.transparent,
+                            Color(0xFF22252D)
+                          ],
+                        ).createShader(
+                            Rect.fromLTRB(0, 0, rect.width, rect.height));
+                      },
+                      blendMode: BlendMode.dstOut,
+                      child: Image.asset(
+                        'assets/images/poster.jpg',
+                        fit: BoxFit.fitWidth,
                       ),
-                    ]),
-                Row(children: [
-                  Container(
-                      child: Padding(
-                          padding:
-                              const EdgeInsets.only(left: 18.0, right: 18.0),
-                          child: Stack(clipBehavior: Clip.none, children: [
-                            Container(
-                                width: MediaQuery.of(context).size.width * 0.85,
-                                child: Text(
-                                  this.title.toUpperCase(),
-                                  softWrap: true,
-                                  textWidthBasis: TextWidthBasis.longestLine,
-                                  style: Theme.of(context).textTheme.titleLarge,
-                                  textAlign: TextAlign.left,
-                                  maxLines: 5,
-                                )),
-                            Positioned(
-                                top: 15,
-                                left: MediaQuery.of(context).size.width * 0.80,
-                                child: LikeButton(
-                                  size: 40,
-                                  likeBuilder: (bool isLiked) {
-                                    return Icon(
-                                      Icons.favorite,
-                                      color:
-                                          isLiked ? leisureColor : Colors.grey,
-                                      size: 40,
-                                    );
-                                  },
-                                  circleColor: CircleColor(
-                                      start: Colors.white, end: leisureColor),
-                                  bubblesColor: BubblesColor(
-                                    dotPrimaryColor: leisureColor,
-                                    dotSecondaryColor: leisureColor,
-                                  ),
-                                  isLiked: this.isFavorite,
-                                  onTap: (isLiked) {
-                                    return Future.delayed(
-                                        Duration(milliseconds: 1), () {
-                                      isLiked = !isLiked;
-                                      return isLiked;
-                                    });
-                                  },
-                                ))
-                          ])))
+                    ),
+                  ),
                 ]),
-              ],
+            Row(children: [
+              Container(
+                  child: Padding(
+                      padding: const EdgeInsets.only(left: 18.0, right: 18.0),
+                      child: Stack(clipBehavior: Clip.none, children: [
+                        Container(
+                            width: MediaQuery.of(context).size.width * 0.85,
+                            child: Text(
+                              this.title.toUpperCase(),
+                              softWrap: true,
+                              textWidthBasis: TextWidthBasis.longestLine,
+                              style: Theme.of(context).textTheme.titleLarge,
+                              textAlign: TextAlign.left,
+                              maxLines: 5,
+                            )),
+                        Positioned(
+                            top: 15,
+                            left: MediaQuery.of(context).size.width * 0.80,
+                            child: LikeButton(
+                              size: 40,
+                              likeBuilder: (bool isLiked) {
+                                return Icon(
+                                  Icons.favorite,
+                                  color: isLiked ? leisureColor : Colors.grey,
+                                  size: 40,
+                                );
+                              },
+                              circleColor: CircleColor(
+                                  start: Colors.white, end: leisureColor),
+                              bubblesColor: BubblesColor(
+                                dotPrimaryColor: leisureColor,
+                                dotSecondaryColor: leisureColor,
+                              ),
+                              isLiked: this.isFavorite,
+                              onTap: (isLiked) {
+                                return Future.delayed(Duration(milliseconds: 1),
+                                    () {
+                                  isLiked = !isLiked;
+                                  return isLiked;
+                                });
+                              },
+                            ))
+                      ])))
+            ]),
+          ],
+        )
+      ]),
+      SizedBox(height: 20),
+      Padding(
+          padding: EdgeInsets.only(left: 18),
+          child: Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+            SizedBox(
+                width: MediaQuery.of(context).size.width * 0.85,
+                height: 50,
+                child: Wrap(
+                    spacing: 7.6,
+                    alignment: WrapAlignment.start,
+                    runSpacing: 7.5,
+                    children: [
+                      LeisureTag(text: "85% love it"),
+                      LeisureTag(text: "Fantasy"),
+                      LeisureTag(text: "2015"),
+                      LeisureTag(text: "Right up your alley!")
+                    ])),
+          ])),
+      SizedBox(height: 35),
+      Row(children: [
+        Padding(
+            padding: EdgeInsets.only(left: 18),
+            child: Text(
+              AppLocalizations.of(context).synopsis,
+              style: Theme.of(context).textTheme.displayMedium,
+            ))
+      ]),
+      SizedBox(height: 7.5),
+      Flexible(
+        child: Padding(
+            padding: EdgeInsets.only(left: 18, right: 18),
+            child: Text(
+              this.synopsis,
+              softWrap: true,
+              textAlign: TextAlign.justify,
+              style: Theme.of(context).textTheme.bodySmall,
+            )),
+      ),
+      SizedBox(height: 35),
+      Row(children: [
+        Padding(
+            padding: EdgeInsets.only(left: 18),
+            child: Text(
+              AppLocalizations.of(context).cast,
+              style: Theme.of(context).textTheme.displayMedium,
+            ))
+      ]),
+      SizedBox(height: 7.5),
+      Row(children: [
+        Padding(
+          padding: EdgeInsets.only(left: 18, right: 18),
+          child: Row(children: [
+            Text(
+              this.cast.join("\n"),
+              softWrap: true,
+              textAlign: TextAlign.justify,
+              style: Theme.of(context).textTheme.bodySmall,
             )
           ]),
-          SizedBox(height: 20),
-          Padding(
-              padding: EdgeInsets.only(left: 18),
-              child: Row(mainAxisAlignment: MainAxisAlignment.start, children: [
-                SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.85,
-                    height: 50,
-                    child: Wrap(
-                        spacing: 7.6,
-                        alignment: WrapAlignment.start,
-                        runSpacing: 7.5,
-                        children: [
-                          LeisureTag(text: "85% love it"),
-                          LeisureTag(text: "Fantasy"),
-                          LeisureTag(text: "2015"),
-                          LeisureTag(text: "Right up your alley!")
-                        ])),
-              ])),
-          SizedBox(height: 35),
-          Row(children: [
-            Padding(
-                padding: EdgeInsets.only(left: 18),
-                child: Text(
-                  AppLocalizations.of(context).synopsis,
-                  style: Theme.of(context).textTheme.displayMedium,
-                ))
+        ),
+      ]),
+      SizedBox(height: 35),
+      Row(children: [
+        Padding(
+            padding: EdgeInsets.only(left: 18),
+            child: Text(
+              AppLocalizations.of(context).length,
+              style: Theme.of(context).textTheme.displayMedium,
+            ))
+      ]),
+      SizedBox(height: 7.5),
+      Row(children: [
+        Padding(
+          padding: EdgeInsets.only(left: 18, right: 18),
+          child: Row(children: [
+            Text(
+              getLength(context),
+              softWrap: true,
+              textAlign: TextAlign.justify,
+              style: Theme.of(context).textTheme.bodySmall,
+            )
           ]),
-          SizedBox(height: 7.5),
-          Flexible(
-            child: Padding(
-                padding: EdgeInsets.only(left: 18, right: 18),
-                child: Text(
-                  this.synopsis,
-                  softWrap: true,
-                  textAlign: TextAlign.justify,
-                  style: Theme.of(context).textTheme.bodySmall,
-                )),
-          ),
-          SizedBox(height: 35),
-          Row(children: [
-            Padding(
-                padding: EdgeInsets.only(left: 18),
-                child: Text(
-                  AppLocalizations.of(context).cast,
-                  style: Theme.of(context).textTheme.displayMedium,
-                ))
-          ]),
-          SizedBox(height: 7.5),
-          Row(children: [
-            Padding(
-              padding: EdgeInsets.only(left: 18, right: 18),
-              child: Row(children: [
-                Text(
-                  this.cast.join("\n"),
-                  softWrap: true,
-                  textAlign: TextAlign.justify,
-                  style: Theme.of(context).textTheme.bodySmall,
-                )
-              ]),
-            ),
-          ]),
-          SizedBox(height: 35),
-          Row(children: [
-            Padding(
-                padding: EdgeInsets.only(left: 18),
-                child: Text(
-                  AppLocalizations.of(context).length,
-                  style: Theme.of(context).textTheme.displayMedium,
-                ))
-          ]),
-          SizedBox(height: 7.5),
-          Row(children: [
-            Padding(
-              padding: EdgeInsets.only(left: 18, right: 18),
-              child: Row(children: [
-                Text(
-                  getLength(context),
-                  softWrap: true,
-                  textAlign: TextAlign.justify,
-                  style: Theme.of(context).textTheme.bodySmall,
-                )
-              ]),
-            ),
-          ])
-        ]));
+        ),
+      ]),
+      SizedBox(height: 100)
+    ]);
   }
 }
