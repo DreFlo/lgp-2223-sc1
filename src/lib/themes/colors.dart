af7307936e94ffce9733fc28aa0a88fd073fd9c0
Margarida Ferreira <up201905046@fe.up.pt>
2023-03-30 18:56:22 +0100

    Add color theme file

---
+++ src/lib/themes/colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+const Color mainPurple = Color(0xFF6C5DD3);
+
+const Color grayButton = Color(0xFF262A34);
+
+const Color appBackground = Color(0xFF181A20);
+
+const Color studentColor = Color(0xFFFFDD72);
+
+const Color leisureColor = Color(0xFFF52349);
+
+const Color personalColor = Color(0xFFB3FF67);
+
+const Color fitnessColor = Color(0xFF52ACFF);
+
