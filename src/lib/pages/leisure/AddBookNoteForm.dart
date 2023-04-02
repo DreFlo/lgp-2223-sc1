2348fdfbb06669442e232dacb7c58fb71fb81827
lessthelonely <42583814+lessthelonely@users.noreply.github.com>
2023-04-02 16:20:28 +0100

    Add Book notes screens.

--- src/lib/pages/leisure/AddBookNoteForm.dart
+++ src/lib/pages/leisure/AddBookNoteForm.dart
@@ -2,10 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:src/themes/colors.dart';
 
-class AddBookNoteForm extends StatefulWidget {
-  int startPage, endPage;
+import '../../utils/enums.dart';
+import 'FinishedMediaForm.dart';
 
-  AddBookNoteForm({Key? key, required this.startPage, required this.endPage}) : super(key: key);
+class AddBookNoteForm extends StatefulWidget {
+  int? startPage, endPage;
+
+  AddBookNoteForm({Key? key, this.startPage, this.endPage}) : super(key: key);
 
   @override
   State<AddBookNoteForm> createState() => _AddBookNoteFormState();
@@ -33,36 +36,185 @@ class _AddBookNoteFormState extends State<AddBookNoteForm> {
       Container(
           padding: const EdgeInsets.only(left: 18),
           child: Column(children: [
-            Text(AppLocalizations.of(context).add_note_callout,
+            Text(AppLocalizations.of(context).progress_callout,
                 softWrap: true,
-                textWidthBasis: TextWidthBasis.longestLine,
                 style: Theme.of(context).textTheme.titleLarge,
                 textAlign: TextAlign.left),
             const SizedBox(height: 10),
           ])),
+      Row(children: [
+        Padding(
+            padding: const EdgeInsets.only(left: 18),
+            child: Text(
+              AppLocalizations.of(context).what_page,
+              style: Theme.of(context).textTheme.displayMedium,
+            ))
+      ]),
+      const SizedBox(height: 7.5),
       Padding(
-          padding: EdgeInsets.only(left: 18),
-          child: SizedBox(
-              width: MediaQuery.of(context).size.width * 0.90,
-              height: 200,
-              child: TextField(
-                  autofocus: true,
-                  style: Theme.of(context).textTheme.bodySmall,
-                  maxLines: 10,
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: lightGray,
-                    helperStyle: Theme.of(context).textTheme.labelSmall,
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide.none,
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                  )))),
+              padding: EdgeInsets.only(left: 18),
+              child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+                SizedBox(
+                    width: ((0.5 * (MediaQuery.of(context).size.width * 0.9)) -
+                        10),
+                    child: TextField(
+                        onChanged: (input) {
+                          setState(() {
+                            widget.startPage = int.parse(input);
+                          });
+                        },
+                        keyboardType: TextInputType.number,
+                        style: Theme.of(context).textTheme.bodySmall,
+                        maxLines: 1,
+                        textAlign: TextAlign.center,
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: lightGray,
+                          helperStyle: Theme.of(context).textTheme.labelSmall,
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                        ))),
+                SizedBox(width: 10),
+                SizedBox(
+                    width: ((0.5 * (MediaQuery.of(context).size.width * 0.9)) -
+                        10),
+                    child: TextField(
+                        onChanged: (input) {
+                          setState(() {
+                            widget.endPage = int.parse(input);
+                          });
+                        },
+                        keyboardType: TextInputType.number,
+                        style: Theme.of(context).textTheme.bodySmall,
+                        maxLines: 1,
+                        textAlign: TextAlign.center,
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: lightGray,
+                          helperStyle: Theme.of(context).textTheme.labelSmall,
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                        ))),
+              ])),
+      const SizedBox(height: 10),
+      Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Padding(
+            padding: const EdgeInsets.only(left: 18),
+            child: TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+
+                  showModalBottomSheet(
+                      context: context,
+                      isScrollControlled: true,
+                      backgroundColor: Color(0xFF22252D),
+                      shape: RoundedRectangleBorder(
+                        borderRadius:
+                            BorderRadius.vertical(top: Radius.circular(30.0)),
+                      ),
+                      builder: (context) => DraggableScrollableSheet(
+                          expand: false,
+                          minChildSize: 0.35,
+                          maxChildSize: 0.75,
+                          builder: (context, scrollController) => Stack(
+                                  alignment: AlignmentDirectional.bottomCenter,
+                                  children: [
+                                    Padding(
+                                        padding: EdgeInsets.only(
+                                            bottom: MediaQuery.of(context)
+                                                    .viewInsets
+                                                    .bottom +
+                                                50),
+                                        child: SingleChildScrollView(
+                                            controller: scrollController,
+                                            child: FinishedMediaForm(
+                                                rating: Reaction.neutral,
+                                                startDate: DateTime.now()
+                                                    .toString()
+                                                    .split(" ")[0],
+                                                endDate: 'Not Defined',
+                                                isFavorite: false))),
+                                    Positioned(
+                                        left: 16,
+                                        right: 16,
+                                        bottom: 16,
+                                        child: Padding(
+                                            padding: EdgeInsets.only(
+                                                bottom: MediaQuery.of(context)
+                                                    .viewInsets
+                                                    .bottom),
+                                            child: ElevatedButton(
+                                              onPressed: () {
+                                                //TODO: Save stuff + send to database.
+                                              },
+                                              style: ElevatedButton.styleFrom(
+                                                minimumSize: Size(
+                                                    MediaQuery.of(context)
+                                                            .size
+                                                            .width *
+                                                        0.95,
+                                                    55),
+                                                backgroundColor: leisureColor,
+                                                shape: RoundedRectangleBorder(
+                                                  borderRadius:
+                                                      BorderRadius.circular(
+                                                          25.0),
+                                                ),
+                                              ),
+                                              child: Text(
+                                                  AppLocalizations.of(context)
+                                                      .save,
+                                                  style: Theme.of(context)
+                                                      .textTheme
+                                                      .headlineSmall),
+                                            )))
+                                  ])));
+                },
+                child: Text(
+                  AppLocalizations.of(context).finished_book,
+                  style: Theme.of(context).textTheme.displayMedium,
+                )))
+      ]),
+      const SizedBox(height: 15),
+      Row(children: [
+        Padding(
+            padding: const EdgeInsets.only(left: 18),
+            child: Text(
+              AppLocalizations.of(context).any_thoughts,
+              style: Theme.of(context).textTheme.displayMedium,
+            ))
+      ]),
+      const SizedBox(height: 7.5),
+      Row(children: [
+        Padding(
+            padding: const EdgeInsets.only(left: 18),
+            child: SizedBox(
+                width: MediaQuery.of(context).size.width * 0.90,
+                height: 200,
+                child: TextField(
+                    style: Theme.of(context).textTheme.bodySmall,
+                    maxLines: 10,
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: lightGray,
+                      helperStyle: Theme.of(context).textTheme.labelSmall,
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide.none,
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ))))
+      ]),
       Padding(
           padding: const EdgeInsets.only(left: 18, top: 30),
           child: ElevatedButton(
               onPressed: () {
-                //TODO: Add functionality for adding note.
+                //TODO: Add functionality for adding note (sending to database).
               },
               style: ElevatedButton.styleFrom(
                 minimumSize: Size(MediaQuery.of(context).size.width * 0.90, 55),
@@ -73,9 +225,6 @@ class _AddBookNoteFormState extends State<AddBookNoteForm> {
               ),
               child: Text(AppLocalizations.of(context).save,
                   style: Theme.of(context).textTheme.headlineSmall))),
-      Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [SizedBox(height: 100)])
-    ]);
+    const SizedBox(height: 150)]);
   }
 }
