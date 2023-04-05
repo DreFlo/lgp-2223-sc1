5043eb6a3e783495cf8bac2d61bc2f34402d567b
melisilva <melissa.silva2001.ms@gmail.com>
2023-04-05 17:08:17 +0100

    Need Padding or something in SeeAll but almost done

--- src/lib/pages/catalog_search/SeeAll.dart
+++ src/lib/pages/catalog_search/SeeAll.dart
@@ -12,34 +12,35 @@ class SeeAll extends StatelessWidget {
     double baseWidth = 390;
     double fem = MediaQuery.of(context).size.width / baseWidth;
     return Scaffold(
-        body: Column(crossAxisAlignment: CrossAxisAlignment.start, 
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start, 
         children: [
-      Padding(
-        padding: EdgeInsets.fromLTRB(10 * fem, 50 * fem, 0, 0),
-        child: Row(
-          children: [
-            IconButton(
-              icon: const Icon(Icons.arrow_back_ios),
-              color: Colors.white,
-              onPressed: () {
-                Navigator.pop(context);
-              },
+          Padding(
+            padding: EdgeInsets.fromLTRB(10 * fem, 50 * fem, 0, 0),
+            child: Row(
+              children: [
+                IconButton(
+                  icon: const Icon(Icons.arrow_back_ios),
+                  color: Colors.white,
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                Text(
+                  title,
+                  style: const TextStyle(
+                    fontFamily: 'Poppins',
+                    fontSize: 16,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w500,
+                    height: 1.5,
+                  ),
+                  textAlign: TextAlign.left,
+                ),
+              ],
             ),
-            Text(
-             title,
-              style: const TextStyle(
-                fontFamily: 'Poppins',
-                fontSize: 16,
-                color: Colors.white,
-                fontWeight: FontWeight.w500,
-                height: 1.5,
-              ),
-              textAlign: TextAlign.left,
-            ),
-          ],
-        ),
-      ),
-      Padding(
+          ),
+          Padding(
             padding: EdgeInsets.fromLTRB(10 * fem, 10 * fem, 0, 0),
             child: Center(
               child: Container(
@@ -72,20 +73,24 @@ class SeeAll extends StatelessWidget {
               ),
             ),
           ),
-      /*SizedBox(
-              height: MediaQuery.of(context).size.height - 100, // or any other height that fits your design
-              child:  SingleChildScrollView(
-              child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 20 * fem),
-                  child: GridView.count(
-                    crossAxisCount: 3,
-                    childAspectRatio: 0.7,
-                    mainAxisSpacing: 22.0 * fem,
-                    crossAxisSpacing: 10.0 * fem,
-                    children: List.generate(media.length, (index) {
-                      return Media(image: media[index]['poster_path']);
-                    }),
-                  ))))*/
-    ]));
+
+          Expanded(
+            child: SingleChildScrollView(
+              child: Wrap(
+                spacing: 10.0 * fem,
+                runSpacing: 22.0 * fem,
+                children: List.generate(media.length, (index) {
+                  return SizedBox(
+                    width: 100.0 * fem,
+                    height: 150.0 * fem,
+                    child: Media(image: media[index]['poster_path']),
+                  );
+                }),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
