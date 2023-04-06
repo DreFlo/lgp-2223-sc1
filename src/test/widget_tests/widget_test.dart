57fc8903f2e6b5191ce4b9277afe3a366d49b50f
Margarida Ferreira <up201905046@fe.up.pt>
2023-04-06 18:20:00 +0100

    Remove unused imports

--- src/test/widget_tests/widget_test.dart
+++ src/test/widget_tests/widget_test.dart
@@ -5,11 +5,7 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/mockito.dart';
-import 'package:src/app.dart';
-import 'package:src/daos/person_dao.dart';
 import 'package:src/utils/service_locator.dart';
 
 import '../utils/service_locator_test_util.dart';
