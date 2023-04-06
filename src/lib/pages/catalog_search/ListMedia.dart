6c74df1c0fc2ecae3c25b7f0006e1da511302d45
melisilva <melissa.silva2001.ms@gmail.com>
2023-04-06 09:46:32 +0100

    Doesn't show media whose image is null

--- src/lib/pages/catalog_search/ListMedia.dart
+++ src/lib/pages/catalog_search/ListMedia.dart
@@ -13,31 +13,37 @@ class ListMedia extends StatelessWidget {
   Widget build(BuildContext context) {
     double baseWidth = 390;
     double fem = MediaQuery.of(context).size.width / baseWidth;
+
+    // Filter out null images
+    List filteredMedia =
+        media.where((item) => showWidget(item) != null).toList();
+
     return Padding(
       padding: EdgeInsets.fromLTRB(40 * fem, 22 * fem, 0, 0),
       child: SingleChildScrollView(
         child: Wrap(
           spacing: 10.0 * fem,
           runSpacing: 22.0 * fem,
-          children: List.generate(media.length, (index) {
+          children: List.generate(filteredMedia.length, (index) {
             return SizedBox(
               width: 100.0 * fem,
               height: 150.0 * fem,
-              child: showWidget(index),
+              child: showWidget(filteredMedia[index]),
             );
           }),
-          )),
+        ),
+      ),
     );
   }
-  showWidget(int index) {
+
+  showWidget(dynamic item) {
     if (title == 'All Books') {
-      if (media[index].info.imageLinks['thumbnail'] != null) {
-        return Book(
-            image: media[index].info.imageLinks['thumbnail'].toString());
+      if (item.info.imageLinks['thumbnail'] != null) {
+        return Book(image: item.info.imageLinks['thumbnail'].toString());
       }
     } else {
-      if(media[index]['poster_path'] != null){
-        return Media(image: media[index]['poster_path']);
+      if (item['poster_path'] != null) {
+        return Media(image: item['poster_path']);
       }
     }
   }
