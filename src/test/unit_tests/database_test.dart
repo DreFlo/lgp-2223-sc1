aee541acf1c45f74707664f3808e5880fbf8e45d
melisilva <melissa.silva2001.ms@gmail.com>
2023-04-06 15:27:00 +0100

    Correction

--- src/test/unit_tests/database_test.dart
+++ src/test/unit_tests/database_test.dart
@@ -39,7 +39,7 @@ import 'package:src/daos/notes/book_note_dao.dart';
 import 'package:src/models/notes/episode_note.dart';
 import 'package:src/daos/notes/episode_note_dao.dart';
 import 'package:src/models/media/video.dart';
-import 'package:src/utils/enums.dart' as enums;
+import 'package:src/utils/enums.dart' as _enums;
 import 'package:src/utils/service_locator.dart';
 import 'package:src/daos/user_dao.dart';
 import 'package:src/models/user.dart';
@@ -285,14 +285,14 @@ void main() {
       await serviceLocator<TaskGroupDao>().insertTaskGroup(TaskGroup(
         name: 'name',
         description: 'description',
-        priority: enums.Priority.high,
+        priority: _enums.Priority.high,
         deadline: DateTime.now(),
       ));
 
       await serviceLocator<TaskDao>().insertTask(Task(
           name: 'name',
           description: 'description',
-          priority: enums.Priority.high,
+          priority: _enums.Priority.high,
           deadline: DateTime.now().subtract(const Duration(days: 1)),
           taskGroupId: 1,
           subjectId: 1,
@@ -974,7 +974,7 @@ void main() {
       expect(users.length, 0);
 
       await serviceLocator<UserDao>()
-          .insertUser(User(userName: 'Emil', password: '1234', xp: 23, imagePath: 'test'));
+        .insertUser(User(userName: 'Emil', password: '1234', xp: 23, imagePath: 'test'));
 
       users = await serviceLocator<UserDao>().findAllUsers();
 
