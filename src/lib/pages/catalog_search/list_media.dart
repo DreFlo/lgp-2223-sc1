6cf2fff9367002046654a2db71601050cf04ee0a
melisilva <melissa.silva2001.ms@gmail.com>
2023-04-10 10:25:04 +0100

    Add conditions for books

--- src/lib/pages/catalog_search/list_media.dart
+++ src/lib/pages/catalog_search/list_media.dart
@@ -286,7 +286,7 @@ class ListMedia extends StatelessWidget {
           child: Text(AppLocalizations.of(context).add,
               style: Theme.of(context).textTheme.headlineSmall),
         );
-      } else {
+      } else if (status == Status.goingThrough || status == Status.planTo) {
         // If media is somehow in the catalog, then user should be able to see their notes and edit info.
         return Container(
           width: MediaQuery.of(context).size.width * 0.95,
@@ -354,6 +354,60 @@ class ListMedia extends StatelessWidget {
                 style: ElevatedButton.styleFrom(
                   minimumSize:
                       Size(MediaQuery.of(context).size.width * 0.45, 55),
+                  backgroundColor: leisureColor,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25.0),
+                  ),
+                ),
+                child: Text(AppLocalizations.of(context).notes,
+                    style: Theme.of(context).textTheme.headlineSmall),
+              )
+            ],
+          ),
+        );
+      } else if (status == Status.done) {
+        // If media is somehow in the catalog, then user should be able to see their notes and edit info.
+        return Container(
+          width: MediaQuery.of(context).size.width,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  showModalBottomSheet(
+                      context: context,
+                      isScrollControlled: true,
+                      backgroundColor: Color(0xFF22252D),
+                      shape: RoundedRectangleBorder(
+                        borderRadius:
+                            BorderRadius.vertical(top: Radius.circular(30.0)),
+                      ),
+                      builder: (context) => DraggableScrollableSheet(
+                          expand: false,
+                          initialChildSize: 0.35,
+                          minChildSize: 0.35,
+                          maxChildSize: 0.5,
+                          builder: (context, scrollController) => Stack(
+                                  alignment: AlignmentDirectional.bottomCenter,
+                                  children: [
+                                    Padding(
+                                        padding: EdgeInsets.only(
+                                            bottom: MediaQuery.of(context)
+                                                    .viewInsets
+                                                    .bottom +
+                                                50),
+                                        child: SingleChildScrollView(
+                                            controller: scrollController,
+                                            child: BookNotesSheet(
+                                                notes: const {},
+                                                review: const {
+                                                  Reaction.dislike: "Hated it."
+                                                })))
+                                  ])));
+                },
+                style: ElevatedButton.styleFrom(
+                  minimumSize:
+                      Size(MediaQuery.of(context).size.width * 0.90, 55),
                   backgroundColor: leisureColor,
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(25.0),
@@ -446,43 +500,46 @@ class ListMedia extends StatelessWidget {
         );
       } else if (status == Status.planTo) {
         return Container(
-          width: MediaQuery.of(context).size.width,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              ElevatedButton(
-                onPressed: () {
-                  showModalBottomSheet(
-                      context: context,
-                      isScrollControlled: true,
-                      backgroundColor: Color(0xFF22252D),
+            width: MediaQuery.of(context).size.width,
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      showModalBottomSheet(
+                          context: context,
+                          isScrollControlled: true,
+                          backgroundColor: Color(0xFF22252D),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.vertical(
+                                top: Radius.circular(30.0)),
+                          ),
+                          builder: (context) => Padding(
+                              padding: EdgeInsets.only(
+                                  bottom:
+                                      MediaQuery.of(context).viewInsets.bottom),
+                              child: Stack(children: const [
+                                FinishedMediaForm(
+                                    isFavorite: false,
+                                    startDate: "04/09/2023",
+                                    endDate: "04/09/2023",
+                                    rating: Reaction.hate),
+                              ])));
+                    },
+                    style: ElevatedButton.styleFrom(
+                      minimumSize:
+                          Size(MediaQuery.of(context).size.width * 0.90, 55),
+                      backgroundColor: leisureColor,
                       shape: RoundedRectangleBorder(
-                        borderRadius:
-                            BorderRadius.vertical(top: Radius.circular(30.0)),
+                        borderRadius: BorderRadius.circular(25.0),
                       ),
-                      builder: (context) => Padding(
-                          padding: EdgeInsets.only(
-                              bottom: MediaQuery.of(context).viewInsets.bottom),
-                          child: Stack(children: const [
-                            FinishedMediaForm(
-                                isFavorite: false,
-                                startDate: "04/09/2023",
-                                endDate: "04/09/2023",
-                                rating: Reaction.hate),
-                          ])));
-                },
-                style: ElevatedButton.styleFrom(
-                  minimumSize:
-                      Size(MediaQuery.of(context).size.width * 0.90, 55),
-                  backgroundColor: leisureColor,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(25.0),
-                  ),
-                ),
-                child: Text(AppLocalizations.of(context).review,
-                    style: Theme.of(context).textTheme.headlineSmall),
-              )]));
-      } else if (status == Status.done) {return Container(
+                    ),
+                    child: Text(AppLocalizations.of(context).review,
+                        style: Theme.of(context).textTheme.headlineSmall),
+                  )
+                ]));
+      } else if (status == Status.done) {
+        return Container(
           width: MediaQuery.of(context).size.width,
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -655,8 +712,7 @@ class ListMedia extends StatelessWidget {
 
   showMediaPageButton(dynamic item, context) {
     if (title == 'All Books') {
-      return mediaPageButton(
-          'Book', Status.goingThrough, context); //get status from DB
+      return mediaPageButton('Book', Status.done, context); //get status from DB
     } else if (title == 'All Movies') {
       return mediaPageButton(
           'Movie', Status.done, context); //get status from DB
