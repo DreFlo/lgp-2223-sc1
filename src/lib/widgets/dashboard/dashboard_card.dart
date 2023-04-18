eb68f514bc77fa1a05857c348a67d49a1e5423af
melisilva <melissa.silva2001.ms@gmail.com>
2023-04-18 21:11:57 +0100

    Dashboard cards with Task/TaskGroup/Event data

--- src/lib/widgets/dashboard/dashboard_card.dart
+++ src/lib/widgets/dashboard/dashboard_card.dart
@@ -1,21 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:src/themes/colors.dart';
+import 'package:src/models/student/task.dart';
+import 'package:src/models/student/task_group.dart';
+import 'package:src/models/timeslot/timeslot_media_timeslot_super_entity.dart';
+import 'package:src/utils/service_locator.dart';
+import 'package:src/models/student/subject.dart';
+import 'package:src/daos/student/subject_dao.dart';
 
 class DashboardCard extends StatefulWidget {
-  final String title;
+  final Task? task; //student
+  final TaskGroup? taskGroup; //student
+  final TimeslotMediaTimeslotSuperEntity? mediaEvent; //media
   final String module;
-  final String subject;
-  final String institution;
-  final int nTasks;
 
   const DashboardCard(
       {Key? key,
-      required this.title,
-      required this.subject,
       required this.module,
-      required this.institution,
-      required this.nTasks})
+      this.task,
+      this.taskGroup,
+      this.mediaEvent})
       : super(key: key);
+
+//nTasks and institution can be added I guess? Only for student
+//Possible to check what's the type of media for media events?
 
   @override
   State<DashboardCard> createState() => _DashboardCardState();
@@ -28,6 +35,56 @@ class _DashboardCardState extends State<DashboardCard> {
     'Personal': personalColor,
     'Fitness': fitnessColor,
   };
+  String subjectOrDate = '';
+
+  @override
+  void initState() {
+    super.initState();
+    getSubjectOrDate().then((value) => setState(() {
+      subjectOrDate = value;
+    }));
+  }
+
+  String getTitle() {
+    if (widget.task != null) {
+      return widget.task!.name;
+    } else if (widget.taskGroup != null) {
+      return widget.taskGroup!.name;
+    } else if (widget.mediaEvent != null) {
+      return widget.mediaEvent!.title;
+    } else {
+      return '';
+    }
+  }
+
+  Future<String> getSubjectOrDate() async{
+    if (widget.task != null) {
+      return await loadSubject();
+    } else if (widget.taskGroup != null) {
+      return await loadSubject();
+    } else if (widget.mediaEvent != null) {
+      String startDate = "${widget.mediaEvent!.startDateTime.day.toString()}/${widget.mediaEvent!.startDateTime.month.toString()}";
+      String endDate = "${widget.mediaEvent!.endDateTime.day.toString()}/${widget.mediaEvent!.endDateTime.month.toString()}";
+      return "$startDate-$endDate";
+    } else {
+      return '';
+    }
+  }
+
+  Future<String> loadSubject() async {
+    int? id;
+    if (widget.task != null) {
+      id = widget.task?.subjectId;
+    } //else if (widget.taskGroup != null) {
+      //id = widget.taskGroup?.subjectId; //when student-backend is merged with this branch
+    //}
+    if(id != null){
+      final subjectStream = serviceLocator<SubjectDao>().findSubjectById(id!);
+      Subject? firstNonNullSubject = await subjectStream.firstWhere((subject) => subject != null, orElse: () => null);
+      return firstNonNullSubject?.name ?? '';
+    }
+    return '';
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -59,7 +116,7 @@ class _DashboardCardState extends State<DashboardCard> {
                       Container(
                           margin: const EdgeInsets.only(bottom: 9),
                           child: Text(
-                            widget.title,
+                            getTitle(),
                             style: Theme.of(context).textTheme.labelLarge,
                           )),
                       Row(
@@ -69,7 +126,7 @@ class _DashboardCardState extends State<DashboardCard> {
                           Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              widget.institution != ''
+                              /*widget.institution != ''
                                   ? Container(
                                       margin: const EdgeInsets.only(bottom: 5),
                                       padding: const EdgeInsets.symmetric(
@@ -89,7 +146,8 @@ class _DashboardCardState extends State<DashboardCard> {
                                             ),
                                       ),
                                     )
-                                  : Container(),
+                                  :*/
+                              Container(),
                               Container(
                                 margin: const EdgeInsets.only(bottom: 5),
                                 padding:
@@ -99,7 +157,7 @@ class _DashboardCardState extends State<DashboardCard> {
                                   color: grayBackground,
                                 ),
                                 child: Text(
-                                  widget.subject,
+                                  subjectOrDate,
                                   style: Theme.of(context)
                                       .textTheme
                                       .headlineLarge
@@ -110,7 +168,7 @@ class _DashboardCardState extends State<DashboardCard> {
                               ),
                             ],
                           ),
-                          widget.nTasks != 0
+                          /*widget.nTasks != 0
                               ? Container(
                                   width: 25,
                                   height: 25,
@@ -124,7 +182,8 @@ class _DashboardCardState extends State<DashboardCard> {
                                               .textTheme
                                               .headlineLarge)),
                                 )
-                              : Container(),
+                              :*/
+                          Container(),
                         ],
                       )
                     ]))));
