231208ca756c274e8085f1ca7511dc16287f2a99
melisilva <melissa.silva2001.ms@gmail.com>
2023-04-19 18:20:04 +0100

    LeisureModule test

--- src/test/widget_tests/widget_test.dart
+++ src/test/widget_tests/widget_test.dart
@@ -5,10 +5,30 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:src/utils/service_locator.dart';
+import 'package:src/pages/catalog_search/leisure_module.dart';
+import 'package:flutter/material.dart';
+import 'package:src/pages/catalog_search/search_bar.dart';
 
 import '../utils/service_locator_test_util.dart';
+
+class LocalizationsInjector extends StatelessWidget {
+  final Widget child;
+  const LocalizationsInjector({Key? key, required this.child})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      home: Material(child: child),
+    );
+  }
+}
 
 void main() {
   setUp(() async {
@@ -37,4 +57,20 @@ void main() {
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
   });*/
+
+  testWidgets('LeisureModule displays two tabs and a search bar', (WidgetTester tester) async {
+    // Build the LeisureModule widget
+    await tester.pumpWidget(const LocalizationsInjector(child: LeisureModule()));
+
+    // Verify that the widget displays two tabs
+    expect(find.text('My Media'), findsOneWidget);
+    expect(find.text('Discover'), findsOneWidget);
+
+    // Tap the "Discover" tab and wait for the widget to rebuild
+    await tester.tap(find.text('Discover'));
+    await tester.pump();
+
+    // Verify that the widget displays a search bar
+    expect(find.widgetWithIcon(SearchBar, Icons.search), findsOneWidget);
+  });
 }
