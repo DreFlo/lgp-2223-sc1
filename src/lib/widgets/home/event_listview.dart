f77a9c9fe3b4056d8808ca91fd9e290da6b3da2c
melisilva <melissa.silva2001.ms@gmail.com>
2023-04-19 11:47:31 +0100

    Event Listview got media and student Events

--- src/lib/widgets/home/event_listview.dart
+++ src/lib/widgets/home/event_listview.dart
@@ -1,25 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:src/models/timeslot/timeslot_media_timeslot_super_entity.dart';
+import 'package:src/models/timeslot/timeslot_student_timeslot_super_entity.dart';
 
-import '../../models/student/task.dart';
 import 'event_card.dart';
 
 class MyEventListView extends StatefulWidget {
-  final List<Task> items;
+  final List<TimeslotMediaTimeslotSuperEntity?>? mediaEvents;
+  final List<TimeslotStudentTimeslotSuperEntity?>? studentEvents;
 
-  const MyEventListView({Key? key, required this.items}) : super(key: key);
+  const MyEventListView({Key? key, this.mediaEvents
+  , this.studentEvents}) : super(key: key);
 
   @override
   State<MyEventListView> createState() => _MyEventListViewState();
 }
 
 class _MyEventListViewState extends State<MyEventListView> {
+  List<dynamic> get items {
+    final List<dynamic> combined = [];
+    if (widget.mediaEvents != null) {
+      combined.addAll(widget.mediaEvents!);
+    }
+    if (widget.studentEvents != null) {
+      combined.addAll(widget.studentEvents!);
+    }
+    return combined;
+  }
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
         scrollDirection: Axis.vertical,
         child: Column(
           children: [
-            for (var item in widget.items)
+            for (var item in items)
               MyEventCard(
                   name: item.name,
                   deadline: item.deadline,
