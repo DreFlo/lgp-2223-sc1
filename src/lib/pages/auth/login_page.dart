69d0d8606ae81b73e0075f14fffa58ca11756d1f
João Barra <joao.jbarra@gmail.com>
2023-04-21 15:54:53 +0100

    Add text inputs and button to login page

--- src/lib/pages/auth/login_page.dart
+++ src/lib/pages/auth/login_page.dart
@@ -9,21 +9,44 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     //TODO: Implement Login page
 
-    return Container(
-        padding: const EdgeInsets.only(top: 15),
-        child: Column(children: [
-          Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            Text(
-              "Login",
-              style: Theme.of(context).textTheme.titleLarge,
-              textAlign: TextAlign.center,
-            ),
-            Text(
-              "No need to be shy... we don’t bite! \nUnless you into that.",
-              style: Theme.of(context).textTheme.labelMedium,
-              textAlign: TextAlign.center,
-            )
-          ])
-        ]));
+    return SizedBox(
+        child: Padding(
+            padding: const EdgeInsets.only(top: 15),
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Text("Login",
+                  style: Theme.of(context).textTheme.titleLarge,
+                  textAlign: TextAlign.left),
+              Text(
+                  "No need to be shy... we don’t bite! \nUnless you into that.",
+                  style: Theme.of(context).textTheme.labelMedium,
+                  textAlign: TextAlign.left),
+              const TextField(
+                  decoration: InputDecoration(
+                border: UnderlineInputBorder(),
+                labelText: 'E-mail',
+              )),
+              const TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    border: UnderlineInputBorder(),
+                    labelText: 'Password',
+                  )),
+              ElevatedButton(
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(primaryColor),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.only(
+                                topRight: Radius.circular(10),
+                                topLeft: Radius.circular(10),
+                                bottomRight: Radius.circular(10),
+                                bottomLeft: Radius.circular(10))))),
+                child: const Text('Sign In'),
+                onPressed: () {
+                  print("Pressed signin btn!");
+                },
+              )
+            ])));
   }
 }
