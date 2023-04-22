5d06430ae930b46a19a0ab1e1b7949f207286d3c
melisilva <melissa.silva2001.ms@gmail.com>
2023-04-22 18:53:56 +0100

    Add WidgetTest for HomePage

--- src/lib/pages/my_home_page.dart
+++ src/lib/pages/my_home_page.dart
@@ -9,66 +9,21 @@ import 'package:src/utils/service_locator.dart';
 import 'package:src/models/timeslot/timeslot_student_timeslot_super_entity.dart';
 import 'package:src/daos/timeslot/timeslot_student_timeslot_super_dao.dart';
 
-import '../models/student/task.dart';
-import '../utils/enums.dart';
 import '../widgets/home/badge_placeholder.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyHomePage> createState() => MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class MyHomePageState extends State<MyHomePage> {
   int _selectedIndex = 0;
   List<TimeslotMediaTimeslotSuperEntity> mediaEvents = [];
   bool loadedAllData = false;
   List<TimeslotStudentTimeslotSuperEntity> studentEvents = [];
   String name = "Joaquim Almeida"; // TODO Get name from database
-
-  List<Task> items = [
-    Task(
-        id: 1,
-        name: 'Ginásio',
-        description: 'Student',
-        deadline: DateTime(2023, 3, 31, 5),
-        priority: Priority.high,
-        taskGroupId: 1,
-        subjectId: 1,
-        xp: 0,
-        finished: false),
-    Task(
-        id: 1,
-        name: 'Kirby & The Forgotten Land',
-        description: 'Leisure',
-        deadline: DateTime(2023, 4, 3, 10),
-        priority: Priority.medium,
-        taskGroupId: 1,
-        subjectId: 1,
-        xp: 0,
-        finished: false),
-    Task(
-        id: 1,
-        name: 'Caminhar',
-        description: 'Personal',
-        deadline: DateTime(2023, 4, 4, 5),
-        priority: Priority.low,
-        taskGroupId: 1,
-        subjectId: 1,
-        xp: 0,
-        finished: false),
-    Task(
-        id: 1,
-        name: 'Treino 5 Minutos',
-        description: 'Fitness',
-        deadline: DateTime(2023, 4, 5, 8),
-        priority: Priority.high,
-        taskGroupId: 1,
-        subjectId: 1,
-        xp: 0,
-        finished: false),
-  ];
 
   @override
   void initState() {
@@ -87,29 +42,6 @@ class _MyHomePageState extends State<MyHomePage> {
       loadedAllData = true;
     });
   }
-
-  /*List<Task> filterItems() {
-    switch (_selectedIndex) {
-      case 1:
-        return items
-            .where((element) => element.description == 'Student')
-            .toList();
-      case 2:
-        return items
-            .where((element) => element.description == 'Leisure')
-            .toList();
-      case 3:
-        return items
-            .where((element) => element.description == 'Fitness')
-            .toList();
-      case 4:
-        return items
-            .where((element) => element.description == 'Personal')
-            .toList();
-      default:
-        return items;
-    }
-  }*/
 
   showWidget() {
     switch (_selectedIndex) {
--- src/test/utils/model_mocks_util.dart
+++ src/test/utils/model_mocks_util.dart
@@ -1,5 +1,5 @@
 import 'package:mockito/annotations.dart';
-
+import 'package:src/models/timeslot/timeslot_student_timeslot_super_entity.dart';
 import 'package:src/models/student/task.dart';
 import 'package:src/models/student/task_group.dart';
 import 'package:src/models/timeslot/timeslot_media_timeslot_super_entity.dart';
@@ -8,5 +8,6 @@ import 'package:src/models/timeslot/timeslot_media_timeslot_super_entity.dart';
   MockSpec<Task>(),
   MockSpec<TaskGroup>(),
   MockSpec<TimeslotMediaTimeslotSuperEntity>(),
+  MockSpec<TimeslotStudentTimeslotSuperEntity>()
 ])
 void main () {}
