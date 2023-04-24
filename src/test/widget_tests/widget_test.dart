bbe6fbe665361f0a7ce3c961e6e18475138e72ba
henriquecscode <henriquecostasousa.hcs@gmail.com>
2023-04-24 09:23:28 +0100

    aggregate of tests was breaking stuff

--- src/test/widget_tests/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'pages/tasks/institution_form_test.dart' as institution_form_test;
-import 'pages/tasks/subject_form_test.dart' as subject_form_test;
-import 'pages/tasks/add_task_note_form_test.dart' as add_task_note_form_test;
-
-void main() {
-  institution_form_test.main();
-  subject_form_test.main();
-  add_task_note_form_test.main();
-}
