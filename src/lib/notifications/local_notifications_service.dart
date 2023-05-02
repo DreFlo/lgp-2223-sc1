31067fa5d66234ec58b2a2afb029e35f787a34da
André Flores <andrejesusferflores@gmail.com>
2023-05-02 17:25:17 +0100

    Create schedule notification function

--- src/lib/notifications/local_notifications_service.dart
+++ src/lib/notifications/local_notifications_service.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:src/themes/colors.dart';
+import 'package:timezone/data/latest.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
+import 'package:flutter_native_timezone/flutter_native_timezone.dart';
 
 class LocalNotificationService {
   // Instance of Flutternotification plugin
   static final FlutterLocalNotificationsPlugin _notificationsPlugin =
       FlutterLocalNotificationsPlugin();
 
-  static void initialize() {
+  static Future<void> initialize() async {
+    await _configureLocalTimeZone();
     // Initialization  setting for android
     const InitializationSettings initializationSettingsAndroid =
         InitializationSettings(
             android: AndroidInitializationSettings("@mipmap/ic_launcher"));
-    _notificationsPlugin.initialize(
+    await _notificationsPlugin.initialize(
       initializationSettingsAndroid,
       // to handle event when we receive notification
       onDidReceiveNotificationResponse: (details) {
@@ -20,7 +24,29 @@ class LocalNotificationService {
       },
     );
   }
-  
+
+  static Future<void> _configureLocalTimeZone() async {
+    tz.initializeTimeZones();
+    final String timeZone = await FlutterNativeTimezone.getLocalTimezone();
+    tz.setLocalLocation(tz.getLocation(timeZone));
+  }
+
+  static tz.TZDateTime _convertTime(DateTime dateTime) {
+    final tz.TZDateTime now = tz.TZDateTime.now(tz.local);
+    tz.TZDateTime scheduleDate = tz.TZDateTime(
+      tz.local,
+      dateTime.year,
+      dateTime.month,
+      dateTime.day,
+      dateTime.hour,
+      dateTime.minute,
+    );
+    if (scheduleDate.isBefore(now)) {
+      // TODO: Maybe better exception
+      throw Exception("Invalid Date Time");
+    }
+    return scheduleDate;
+  }
 
   static Future<void> display(String message) async {
     // To display the notification in device
@@ -29,23 +55,23 @@ class LocalNotificationService {
 
       NotificationDetails notificationDetails = const NotificationDetails(
         android: AndroidNotificationDetails(
-            "Channel Id", 
-            "Main Channel",
-            groupKey: "gfg",
-            category: AndroidNotificationCategory.service,
-            color: primaryColor,
-            ledColor: primaryColor,
-            ledOnMs: 1000,
-            ledOffMs: 500,
-            colorized: true,
-            importance: Importance.max,
-            playSound: true,
-            priority: Priority.max,
-            timeoutAfter: 30000,
-            //autoCancel: false,
-            //ongoing: true
-            ),
-            //to make it persistent you need autoCancel: false and ongoing: true
+          "Channel Id",
+          "Main Channel",
+          groupKey: "gfg",
+          category: AndroidNotificationCategory.service,
+          color: primaryColor,
+          ledColor: primaryColor,
+          ledOnMs: 1000,
+          ledOffMs: 500,
+          colorized: true,
+          importance: Importance.max,
+          playSound: true,
+          priority: Priority.max,
+          timeoutAfter: 30000,
+          //autoCancel: false,
+          //ongoing: true
+        ),
+        //to make it persistent you need autoCancel: false and ongoing: true
       );
       await _notificationsPlugin.show(id, message, message, notificationDetails,
           payload: message);
@@ -53,6 +79,43 @@ class LocalNotificationService {
       debugPrint(e.toString());
     }
   }
+
+  static Future<void> scheduleNotification(
+      int id, String title, DateTime dateTime, String body,
+      {NotificationDetails? details}) async {
+    await _notificationsPlugin.zonedSchedule(
+      id,
+      title,
+      body,
+      _convertTime(dateTime),
+      details ?? const NotificationDetails(
+        android: AndroidNotificationDetails(
+          "Channel Id",
+          "Main Channel",
+          groupKey: "gfg",
+          category: AndroidNotificationCategory.service,
+          color: primaryColor,
+          ledColor: primaryColor,
+          ledOnMs: 1000,
+          ledOffMs: 500,
+          colorized: true,
+          importance: Importance.max,
+          playSound: true,
+          priority: Priority.max,
+          timeoutAfter: 30000,
+          //autoCancel: false,
+          //ongoing: true
+        ),
+        //to make it persistent you need autoCancel: false and ongoing: true
+      ),
+      uiLocalNotificationDateInterpretation: UILocalNotificationDateInterpretation.absoluteTime,
+      matchDateTimeComponents: DateTimeComponents.time,
+      androidAllowWhileIdle: true,
+    );
+  }
+
+  Future<void> cancelAll() async => await _notificationsPlugin.cancelAll();
+  Future<void> cancel(int id) async => await _notificationsPlugin.cancel(id);
 }
 
 class DynamicDialog extends StatefulWidget {
