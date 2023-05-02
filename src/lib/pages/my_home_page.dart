6526550ef13b4b0d672e934ac7c4977b14b460ad
melisilva <melissa.silva2001.ms@gmail.com>
2023-05-02 10:31:05 +0100

    Verifies that student event is finished

--- src/lib/pages/my_home_page.dart
+++ src/lib/pages/my_home_page.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:src/daos/student/task_dao.dart';
+import 'package:src/daos/timeslot/media_media_timeslot_dao.dart';
+import 'package:src/daos/timeslot/task_student_timeslot_dao.dart';
+import 'package:src/models/student/task.dart';
 import 'package:src/pages/gamification/student_timeslot_finished_modal.dart';
+import 'package:src/themes/colors.dart';
 import 'package:src/widgets/home/homepage_horizontal_scrollview.dart';
 import 'package:src/widgets/home/profile_pic.dart';
 import 'package:src/widgets/home/event_listview.dart';
@@ -24,6 +29,10 @@ class _MyHomePageState extends State<MyHomePage> {
   List<TimeslotMediaTimeslotSuperEntity> mediaEvents = [];
   bool loadedAllData = false;
   List<TimeslotStudentTimeslotSuperEntity> studentEvents = [];
+  List<TimeslotStudentTimeslotSuperEntity> finishedStudentEvents = [];
+  List<Task> tasksFinishedEvent = [];
+  Map<int, List<Task>> tasksFinishedEventMap = {};
+  List<int> tasksIdsFinishedEvent = [];
   String name = "Joaquim Almeida"; // TODO Get name from database
 
   @override
@@ -39,27 +48,61 @@ class _MyHomePageState extends State<MyHomePage> {
         .findAllTimeslotMediaTimeslot(start);
     studentEvents = await serviceLocator<TimeslotStudentTimeslotSuperDao>()
         .findAllTimeslotStudentTimeslot(start);
+    finishedStudentEvents =
+        await serviceLocator<TimeslotStudentTimeslotSuperDao>()
+            .findAllFinishedTimeslotStudentTimeslot(now);
+    tasksFinishedEventMap = await getTaskIds();
     setState(() {
       mediaEvents = mediaEvents;
       studentEvents = studentEvents;
+      finishedStudentEvents = finishedStudentEvents;
+      tasksFinishedEventMap = tasksFinishedEventMap;
       loadedAllData = true;
+      checkEventDone();
     });
   }
 
-  void checkEventDone(){
-    studentEvents.sort((a, b) => a.endDateTime.compareTo(b.endDateTime));
-    for(var i=0; i<studentEvents.length; i++){
-      if(studentEvents[i].endDateTime.isBefore(DateTime.now())){
+  Future<List<int>> getMediaIds(int id) async {
+    return await serviceLocator<MediaMediaTimeslotDao>()
+        .findMediaIdByMediaTimeslotId(id);
+  }
 
+  Future<Map<int, List<Task>>> getTaskIds() async {
+    Map<int, List<Task>> tasksFinishedTimeslotMap = {};
+    List<int> taskIds = [];
+    List<Task> tasks = [];
+    for (int i = 0; i < finishedStudentEvents.length; i++) {
+      taskIds = [];
+      taskIds = await serviceLocator<TaskStudentTimeslotDao>()
+          .findTaskIdByStudentTimeslotId(finishedStudentEvents[i].id ?? 0);
+      tasks = [];
+      for (int i = 0; i < taskIds.length; i++) {
+        tasks.add(await serviceLocator<TaskDao>().findTaskById(taskIds[i]).first
+            as Task);
       }
+      tasksFinishedTimeslotMap[finishedStudentEvents[i].id ?? 0] = tasks;
     }
+    return tasksFinishedTimeslotMap;
+  }
 
-    mediaEvents.sort((a, b) => a.endDateTime.compareTo(b.endDateTime));
-    for(var i=0; i<mediaEvents.length; i++){
-      if(mediaEvents[i].endDateTime.isBefore(DateTime.now())){
-      }
+  void showFinishedStudentTimeslot(context, studentTimeslot, tasks) {
+    showDialog(
+        barrierDismissible: false,
+        context: context,
+        builder: (context) => Dialog(
+            backgroundColor: modalBackground,
+            insetPadding: const EdgeInsets.symmetric(horizontal: 20),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30),
+            ),
+            child: StudentTimeslotFinishedModal(
+                timeslot: studentTimeslot, tasks: tasks)));
+  }
+
+  void checkEventDone()  {
+    for(int i= 0; i<finishedStudentEvents.length; i++){
+        showFinishedStudentTimeslot(context, finishedStudentEvents[i], tasksFinishedEventMap[finishedStudentEvents[i].id ?? 0] ?? []);
     }
-
   }
 
   showWidget() {
