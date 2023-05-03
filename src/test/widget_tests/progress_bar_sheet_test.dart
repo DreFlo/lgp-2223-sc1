0458e4276b751dd3c30744ab022f77a30aac89f8
melisilva <melissa.silva2001.ms@gmail.com>
2023-05-03 10:07:37 +0100

    Fix Progress bar test

--- src/test/widget_tests/progress_bar_sheet_test.dart
+++ src/test/widget_tests/progress_bar_sheet_test.dart
@@ -1,13 +1,25 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:mockito/mockito.dart';
+import 'package:src/daos/media/media_dao.dart';
+import 'package:src/daos/student/task_dao.dart';
 import 'package:src/pages/gamification/progress_bar_sheet.dart';
+import 'package:src/utils/service_locator.dart';
 
 import '../utils/locations_injector.dart';
+import '../utils/service_locator_test_util.dart';
 
 class MockImagePicker extends Mock implements ImagePicker {}
 
 void main() {
+  setUp(() async {
+    setupMockServiceLocatorUnitTests();
+    await serviceLocator.allReady();
+  });
+
+  tearDown(() async {
+    await serviceLocator.reset();
+  });
   group('ProgressBarSheet', () {
     late LocalizationsInjector progressBarSheet;
 
@@ -21,13 +33,20 @@ void main() {
     });
 
     testWidgets('displays the user name', (WidgetTester tester) async {
+      //Mock data
+
+      final mockMediaDao = serviceLocator.get<MediaDao>();
+      when(mockMediaDao.countAllMedia()).thenAnswer((_) async => 8);
+      when(mockMediaDao.countFavoriteMedia(true)).thenAnswer((_) async => 2);
+
+      final mockTaskDao = serviceLocator.get<TaskDao>();
+      when(mockTaskDao.countFinishedTaskGroups(true))
+          .thenAnswer((_) async => 8);
+      when(mockTaskDao.countFinishedTasks(true)).thenAnswer((_) async => 2);
+
       await tester.pumpWidget(progressBarSheet);
       final nameFinder = find.text('test_user');
       expect(nameFinder, findsOneWidget);
-    });
-
-    testWidgets('displays the level', (WidgetTester tester) async {
-      await tester.pumpWidget(progressBarSheet);
       final levelFinder = find.text('LEVEL 1');
       expect(levelFinder, findsOneWidget);
     });
