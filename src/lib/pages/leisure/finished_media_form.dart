94dbfcf4d460a87fb10d9c0aadce1f6d9194f1e2
melisilva <melissa.silva2001.ms@gmail.com>
2023-05-04 21:48:30 +0100

    Fix overflow in catalog

--- src/lib/pages/leisure/finished_media_form.dart
+++ src/lib/pages/leisure/finished_media_form.dart
@@ -352,7 +352,8 @@ class _FinishedMediaFormState extends State<FinishedMediaForm> {
                                   fontWeight: FontWeight.normal,
                                   fontSize: 16,
                                 ))),
-                        VerticalDivider(color: modalBackground, thickness: 10),
+                        const VerticalDivider(
+                            color: modalLightBackground, thickness: 10),
                         ElevatedButton(
                             onPressed: () {
                               isFavorite = false;
