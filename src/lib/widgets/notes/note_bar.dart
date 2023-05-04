019ea53b6e60b224fd136f67ea140fcd815e7639
lessthelonely <42583814+lessthelonely@users.noreply.github.com>
2023-05-04 15:01:11 +0100

    Starting work on note bar.

--- src/lib/widgets/notes/note_bar.dart
+++ src/lib/widgets/notes/note_bar.dart
@@ -43,16 +43,15 @@ class _NoteBarState extends State<NoteBar> {
   Widget build(BuildContext context) {
     return InkWell(
         child: Container(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
             width: MediaQuery.of(context).size.width * 0.9,
             decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(10),
                 color: selected ? grayButton : lightGray),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            child: Padding(padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15), child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                Flexible(
-                    flex: 3,
+                Expanded(
+                    flex: 2,
                     child: Column(
                         mainAxisAlignment: MainAxisAlignment.start,
                         crossAxisAlignment: CrossAxisAlignment.start,
@@ -63,43 +62,63 @@ class _NoteBarState extends State<NoteBar> {
                               style: TextStyle(
                                   color: selected ? Colors.black : Colors.white,
                                   fontSize: 16,
+                                  fontWeight: FontWeight.bold)),
+                          Text(note.content,
+                              softWrap: true,
+                              textAlign: TextAlign.justify,
+                              style: TextStyle(
+                                  color: selected ? Colors.black : Colors.white,
+                                  fontSize: 16,
                                   fontWeight: FontWeight.normal))
                         ])),
-                Flexible(
+                Expanded(
                     flex: 1,
-                    child: Column(mainAxisSize: MainAxisSize.min, children: [
-                      ElevatedButton(
-                          child: const Icon(Icons.delete),
-                          onPressed: () {
-                            if (selected) {
-                              onUnselected(note);
-                            } else {
-                              onSelected(note);
-                            }
-                            setState(() {
-                              selected = !selected;
-                            });
-                          }),
-                      ElevatedButton(
-                          child: const Icon(Icons.edit),
-                          onPressed: () {
-                            showModalBottomSheet(
-                                context: context,
-                                isScrollControlled: true,
-                                backgroundColor: const Color(0xFF22252D),
-                                shape: const RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.vertical(
-                                      top: Radius.circular(30.0)),
-                                ),
-                                builder: (builder) => SingleChildScrollView(
-                                    child: AddTaskNoteForm(
-                                        note: note,
-                                        taskId: taskId,
-                                        callback: editNote,
-                                        deleteNoteCallback: deleteNote)));
-                          })
+                    child: Column(mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.end, children: [
+                      Row(children: [
+                        ElevatedButton(
+                            style: ButtonStyle(
+                                shape: MaterialStateProperty.all(
+                                    const CircleBorder()),
+                                padding: MaterialStateProperty.all(
+                                    const EdgeInsets.all(10))),
+                            onPressed: () {
+                              if (selected) {
+                                onUnselected(note);
+                              } else {
+                                onSelected(note);
+                              }
+                              setState(() {
+                                selected = !selected;
+                              });
+                            },
+                            child: const Icon(Icons.delete)),
+                        ElevatedButton(
+                            style: ButtonStyle(
+                                shape: MaterialStateProperty.all(
+                                    const CircleBorder()),
+                                padding: MaterialStateProperty.all(
+                                    const EdgeInsets.all(10))),
+                            onPressed: () {
+                              showModalBottomSheet(
+                                  context: context,
+                                  isScrollControlled: true,
+                                  backgroundColor: const Color(0xFF22252D),
+                                  shape: const RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.vertical(
+                                        top: Radius.circular(30.0)),
+                                  ),
+                                  builder: (builder) => SingleChildScrollView(
+                                      child: AddTaskNoteForm(
+                                          note: note,
+                                          taskId: taskId,
+                                          callback: editNote,
+                                          deleteNoteCallback: deleteNote)));
+                            },
+                            child: const Icon(Icons.edit))
+                      ])
                     ]))
               ],
-            )));
+            ))));
   }
 }
