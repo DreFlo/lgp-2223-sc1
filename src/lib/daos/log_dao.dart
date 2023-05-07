3c241b662423628846d9b3808a0dc46dd0b9ad17
melisilva <melissa.silva2001.ms@gmail.com>
2023-05-07 17:52:23 +0100

    LogDao created

---
+++ src/lib/daos/log_dao.dart
@@ -0,0 +1,26 @@
+import 'package:floor/floor.dart';
+import 'package:src/models/log.dart';
+
+@dao
+abstract class LogDao {
+  @Query('SELECT * FROM log')
+  Future<List<Log>> findAllLogs();
+
+  @Query('SELECT * FROM log WHERE id = :id')
+  Future<Log?> findLogById(int id);
+
+  @insert
+  Future<int> insertLog(Log log);
+
+  @insert
+  Future<void> insertLogs(List<Log> logs);
+
+  @update
+  Future<void> updateLog(Log log);
+
+  @update
+  Future<void> updateLogs(List<Log> logs);
+
+  @delete
+  Future<void> deleteLog(Log log);
+}
