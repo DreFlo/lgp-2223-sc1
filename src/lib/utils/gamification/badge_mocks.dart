96e03bb3a1a00510a41e9e8324416b26571b573c
lessthelonely <42583814+lessthelonely@users.noreply.github.com>
2023-05-08 13:48:12 +0100

    badge 1 mock

---
+++ src/lib/utils/gamification/badge_mocks.dart
@@ -0,0 +1,13 @@
+const badgeTwo = {
+  "title": 'Bare Minimum Bob',
+  "colors": ['FF5DD3C5', 'FF0094FF'],
+  "description": 'Completed your first task!',
+  "icon": 'FontAwesome.face_flushed'
+};
+
+const badgeThree = {
+  "title": 'Eu Tenho Dois Amores...\n(oh boy, you’d be laughing so hard if you knew this reference)',
+  "colors": ['FFD10000', 'FFFF005C'],
+  "description": 'Favorited 2 different media!',
+  "icon": 'FontAwesome.face_grin_hearts'
+};
