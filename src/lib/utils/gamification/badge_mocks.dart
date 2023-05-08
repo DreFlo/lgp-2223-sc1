4abe562c8fd059b2bc2a191e4f9a73c68622ffff
lessthelonely <42583814+lessthelonely@users.noreply.github.com>
2023-05-08 16:14:40 +0100

    Add facts to my mocks.

--- src/lib/utils/gamification/badge_mocks.dart
+++ src/lib/utils/gamification/badge_mocks.dart
@@ -1,7 +1,8 @@
 const badgeOne = {
   "title": 'Chain Streaker',
   "description": '7 consecutive days of activity.',
-  "icon": 'FontAwesome.fire'
+  "icon": 'FontAwesome.fire',
+  "fact": 'Male quokkas will defend their pregnant mate, but once the child is born, they turn into deadbeats.'
 };
 
 const badgeOneColors = "FFFF7B51,FFFF8A00";
@@ -9,7 +10,8 @@ const badgeOneColors = "FFFF7B51,FFFF8A00";
 const badgeTwo = {
   "title": 'Bare Minimum Bob',
   "description": 'Completed your first task!',
-  "icon": 'FontAwesome.face_flushed'
+  "icon": 'FontAwesome.face_flushed',
+  "fact": 'They hop into shops on Rottnest Island regularly to say hello to everybody.'
 };
 
 const badgeTwoColors = "FF5DD3C5,FF0094FF";
@@ -18,7 +20,8 @@ const badgeThree = {
   "title":
       'Eu Tenho Dois Amores...\n(oh boy, you’d be laughing so hard if you knew this reference)',
   "description": 'Favorited 2 different media!',
-  "icon": 'FontAwesome.face_grin_hearts'
+  "icon": 'FontAwesome.face_grin_hearts',
+  "fact": 'Quokkas can travel at speeds of up to 20 miles per hour.'
 };
 
 const badgeThreeColors = "FFD10000,FFFF005C";
