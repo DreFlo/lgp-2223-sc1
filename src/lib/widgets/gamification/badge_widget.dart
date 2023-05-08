fd7f735b2e1919a36776aa638df2a059a10368ce
lessthelonely <42583814+lessthelonely@users.noreply.github.com>
2023-05-08 13:39:04 +0100

    New badge widget.

---
+++ src/lib/widgets/gamification/badge_widget.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:icons_plus/icons_plus.dart';
+
+
+class BadgeWidget extends StatefulWidget {
+  final String title;
+  final String description;
+  final List<String> colors;
+  final String icon;
+
+  const BadgeWidget(
+      {Key? key,
+      required this.title,
+      required this.description,
+      required this.colors,
+      required this.icon})
+      : super(key: key);
+
+  @override
+  State<BadgeWidget> createState() => _BadgeWidgetState();
+}
+
+class _BadgeWidgetState extends State<BadgeWidget> with TickerProviderStateMixin {
+  @override
+  initState() {
+    super.initState();
+  }
+
+  List<Color> getColors() {
+    List<Color> colors = [];
+    for (String color in widget.colors) {
+      colors.add(Color(int.parse(color, radix: 16)));
+    }
+    return colors;
+  }
+
+  IconData eval(String icon) {
+    if (icon == 'FontAwesome.fire') {
+      return FontAwesome.fire;
+    } else if (icon == 'FontAwesome.face_flushed') {
+      return FontAwesome.face_flushed;
+    } else if (icon == 'FontAwesome.face_grin_hearts') {
+      return FontAwesome.face_grin_hearts;
+    }
+    return Icons.error_outline_rounded;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: 100,
+        width: 100,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: getColors(),
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight),
+            shape: BoxShape.circle,
+            color: Colors.white),
+        child: Icon(eval(widget.icon), size: 50, color: Colors.white));
+  }
+}
