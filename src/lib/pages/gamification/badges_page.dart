7a8a52a7bdb145b962eb9bb43685566cc38e64e5
melisilva <melissa.silva2001.ms@gmail.com>
2023-05-08 14:45:21 +0100

    Fix overflow error in badges_page

--- src/lib/pages/gamification/badges_page.dart
+++ src/lib/pages/gamification/badges_page.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:src/themes/colors.dart';
@@ -38,45 +37,58 @@ class _BadgesPageState extends State<BadgesPage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        color: modalLightBackground,
-        child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 18),
-            child: Wrap(spacing: 10, children: [
-              Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+      color: modalLightBackground,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 18),
+        child: Wrap(
+          spacing: 10,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
                 Padding(
-                    padding: const EdgeInsets.only(top: 15, bottom: 15),
-                    child: Container(
-                      width: 115,
-                      height: 18,
-                      alignment: Alignment.center,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: const Color(0xFF414554),
-                      ),
-                    ))
-              ]),
-              const SizedBox(height: 10),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    AppLocalizations.of(context).badges,
-                    style: const TextStyle(
-                        color: Colors.white,
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold),
-                  )
-                ],
-              ),
-              const SizedBox(height: 30),
-              Row(
-                children: [
-                  GridView.count(
+                  padding: const EdgeInsets.only(top: 15, bottom: 15),
+                  child: Container(
+                    width: 115,
+                    height: 18,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: const Color(0xFF414554),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  AppLocalizations.of(context).badges,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 30),
+            Row(
+              children: [
+                Flexible(
+                  child: GridView.count(
                     crossAxisCount: 3,
-                    children: getBadges()
-                  )
-                ],
-              )
-            ])));
+                    shrinkWrap: true,
+                    children: getBadges(),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
