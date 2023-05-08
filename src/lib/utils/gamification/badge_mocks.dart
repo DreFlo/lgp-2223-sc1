36cd5979975634c1dcd664fef9373265e1fff0ec
lessthelonely <42583814+lessthelonely@users.noreply.github.com>
2023-05-08 13:48:24 +0100

    other badges

--- src/lib/utils/gamification/badge_mocks.dart
+++ src/lib/utils/gamification/badge_mocks.dart
@@ -1,3 +1,10 @@
+const badgeOne = {
+  "title": 'Chain Streaker',
+  "colors": ['FFFF7B51', 'FFFF8A00'],
+  "description": '7 consecutive days of activity.',
+  "icon": 'FontAwesome.fire'
+};
+
 const badgeTwo = {
   "title": 'Bare Minimum Bob',
   "colors": ['FF5DD3C5', 'FF0094FF'],
