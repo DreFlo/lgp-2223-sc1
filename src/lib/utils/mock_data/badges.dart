9de46fe8d50964eb0e44827fd45422de84518ee5
melisilva <melissa.silva2001.ms@gmail.com>
2023-05-08 15:01:17 +0100

    Updated mock_data for badges

--- src/lib/utils/mock_data/badges.dart
+++ src/lib/utils/mock_data/badges.dart
@@ -4,25 +4,25 @@ final mockBadges = [
   Badge(
       id: 1,
       name: 'Bare Minimum Bob',
-      description: 'Completed your first task',
-      icon: 'person',
-      colors: '0x5DD3C5',
+      description: 'Completed your first task!',
+      icon: 'FontAwesome.face_flushed',
+      colors: 'FF5DD3C5,FF0094FF',
       fact:
           'Male quokkas will defend their pregnant mate, but once the child is born, they turn into deadbeats.'),
   Badge(
       id: 2,
       name:
-          'Eu Tenho Dois Amores...(oh boy, you\'d be laughing so hard if you knew this reference)',
+          'Eu Tenho Dois Amores...\n(oh boy, you’d be laughing so hard if you knew this reference)',
       description: 'Favorited 2 different media!',
-      icon: 'heart',
-      colors: '0x5DD3C5',
+      icon: 'FontAwesome.face_grin_hearts',
+      colors: 'FFD10000,FFFF005C',
       fact:
           'They hop into shops on Rottnest Island regularly to say hello to everybody.'),
   Badge(
       id: 3,
       name: 'Chain Streaker',
-      description: '7 consecutive days of activity',
-      icon: 'flame',
-      colors: '0x5DD3C5',
+      description: '7 consecutive days of activity.',
+      icon: 'FontAwesome.fire',
+      colors: 'FFFF7B51,FFFF8A00',
       fact: 'Quokkas can travel at speeds of up to 20 miles per hour.'),
 ];
