c6427c2814e31133261fc6d2b3d3ec4b8946aaa5
lessthelonely <42583814+lessthelonely@users.noreply.github.com>
2023-05-08 14:53:38 +0100

    Badge mocks update for color list in string.

--- src/lib/utils/gamification/badge_mocks.dart
+++ src/lib/utils/gamification/badge_mocks.dart
@@ -1,20 +1,24 @@
 const badgeOne = {
   "title": 'Chain Streaker',
-  "colors": ['FFFF7B51', 'FFFF8A00'],
   "description": '7 consecutive days of activity.',
   "icon": 'FontAwesome.fire'
 };
 
+const badgeOneColors = "FFFF7B51,FFFF8A00";
+
 const badgeTwo = {
   "title": 'Bare Minimum Bob',
-  "colors": ['FF5DD3C5', 'FF0094FF'],
   "description": 'Completed your first task!',
   "icon": 'FontAwesome.face_flushed'
 };
 
+const badgeTwoColors = "FF5DD3C5,FF0094FF";
+
 const badgeThree = {
-  "title": 'Eu Tenho Dois Amores...\n(oh boy, you’d be laughing so hard if you knew this reference)',
-  "colors": ['FFD10000', 'FFFF005C'],
+  "title":
+      'Eu Tenho Dois Amores...\n(oh boy, you’d be laughing so hard if you knew this reference)',
   "description": 'Favorited 2 different media!',
   "icon": 'FontAwesome.face_grin_hearts'
 };
+
+const badgeThreeColors = "FFD10000,FFFF005C";
