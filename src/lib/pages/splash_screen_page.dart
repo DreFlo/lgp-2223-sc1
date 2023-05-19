7bba373e5984d206f5194f2650debb2fef5e823e
João Barra <joao.jbarra@gmail.com>
2023-05-19 12:05:22 +0100

    Add wokka logo to splash screen

--- src/lib/pages/splash_screen_page.dart
+++ src/lib/pages/splash_screen_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:src/themes/colors.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class SplashScreenPage extends StatelessWidget {
   const SplashScreenPage({Key? key}) : super(key: key);
@@ -10,11 +11,20 @@ class SplashScreenPage extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Text(
-            "SPLASH SCREEN",
-            style: Theme.of(context).textTheme.titleLarge,
-            textAlign: TextAlign.center,
+          // Text(
+          //   "SPLASH SCREEN",
+          //   style: Theme.of(context).textTheme.titleLarge,
+          //   textAlign: TextAlign.center,
+          // ),
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 0.25,
+            height: MediaQuery.of(context).size.height * 0.25,
+            child: SvgPicture.asset('assets/icons/wokka_mascot.svg'),
           ),
+          Padding(
+              padding: EdgeInsets.only(
+                  bottom: MediaQuery.of(context).size.height * 0.015)),
+          SvgPicture.asset('assets/icons/wokka_title.svg')
         ],
       ),
     );
