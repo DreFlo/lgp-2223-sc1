e8295a3c14f39a47221e550917db8fa5b74bc101
Margarida Ferreira <up201905046@fe.up.pt>
2023-05-24 23:58:07 +0100

    Fix horizontal project scroll

--- src/lib/widgets/weekly_report/project_horizontal_scrollview.dart
+++ src/lib/widgets/weekly_report/project_horizontal_scrollview.dart
@@ -6,45 +6,73 @@ import 'package:src/widgets/weekly_report/project_card.dart';
 class ProjectHorizontalScrollview extends StatelessWidget {
   final List<TaskGroup> contributedTaskGroups;
 
-  const ProjectHorizontalScrollview({super.key, required this.contributedTaskGroups});
+  const ProjectHorizontalScrollview(
+      {super.key, required this.contributedTaskGroups});
 
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
         scrollDirection: Axis.horizontal,
-        child: Row(
-          children: [
-            for (int i = 0;
-            i <
-                contributedTaskGroups
-                    .length;
-            i++)
-              Container(
-                width: 100,
-                height: 120,
-                margin: const EdgeInsets
-                    .symmetric(
-                    horizontal: 5),
-                decoration: BoxDecoration(
-                  borderRadius:
-                  BorderRadius
-                      .circular(5),
-                  color: grayButton,
-                  boxShadow: const [
-                    BoxShadow(
-                      color: studentColor,
-                      offset:
-                      Offset(-5, -5),
+        child: SizedBox(
+            height: 130,
+            child: Row(
+              children: [
+                Container(
+                  width: 100,
+                  height: 120,
+                  margin: const EdgeInsets.symmetric(horizontal: 5),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                    color: grayButton,
+                    boxShadow: const [
+                      BoxShadow(
+                        color: studentColor,
+                        offset: Offset(-5, -5),
+                      ),
+                    ],
+                  ),
+                  child: ProjectCard(
+                    taskGroup: contributedTaskGroups[0],
+                  ),
+                ),
+                Container(
+                  width: 100,
+                  height: 120,
+                  margin: const EdgeInsets.symmetric(horizontal: 5),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                    color: grayButton,
+                    boxShadow: const [
+                      BoxShadow(
+                        color: studentColor,
+                        offset: Offset(-5, -5),
+                      ),
+                    ],
+                  ),
+                  child: ProjectCard(
+                    taskGroup: contributedTaskGroups[0],
+                  ),
+                ),
+                for (int i = 0; i < contributedTaskGroups.length; i++)
+                  Container(
+                    width: 100,
+                    height: 120,
+                    margin: const EdgeInsets.symmetric(horizontal: 5),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5),
+                      color: grayButton,
+                      boxShadow: const [
+                        BoxShadow(
+                          color: studentColor,
+                          offset: Offset(-5, -5),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-                child: ProjectCard(
-                  taskGroup:
-                  contributedTaskGroups[
-                  i],
-                ),
-              )
-          ],
-        ));
+                    child: ProjectCard(
+                      taskGroup: contributedTaskGroups[i],
+                    ),
+                  )
+              ],
+            )));
   }
 }
